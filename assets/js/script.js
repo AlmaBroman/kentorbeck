3ac906cb913eb353fef7f90d8b790c41a77610f8
AlmaBroman <>
2023-10-17 12:45:36 +0000

    Add javascript to pop up

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+let popUp = document.getElementById("pop-up-box");
+let popUpButton = document.getElementById("open-pop-up");
+let popUpClose = document.getElementsByClassName("close-pop-up")[0];
+
+popUpButton.onclick = function() {
+    popUp.style.display = "block";
+}
+
+popUpClose.onclick = function() {
+    popUp.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == popUp) {
+        popUp.style.display = "none";
+    }
+}
