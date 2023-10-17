023ecf93c73e5cdd4dde910b6be5426208e8dd08
AlmaBroman <>
2023-10-17 14:18:58 +0000

    add script to start game

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,11 @@
 let popUp = document.getElementById("pop-up-box");
 let popUpButton = document.getElementById("open-pop-up");
 let popUpClose = document.getElementsByClassName("close-pop-up")[0];
+const startButton = document.getElementById("start-button");
+const gameTitle = document.getElementsByClassName("game-title")[0];
+const startPage = document.getElementsByClassName("start-page")[0];
+const questionContainer = document.getElementsByClassName("question-container")[0];
+const scoreContainer = document.getElementsByClassName("score-container")[0];
 
 popUpButton.onclick = function() {
     popUp.style.display = "block";
@@ -15,3 +20,10 @@ window.onclick = function(event) {
         popUp.style.display = "none";
     }
 }
+
+startButton.addEventListener("click", function() {
+    gameTitle.classList.add("hide");
+    startPage.classList.add("hide");
+    questionContainer.classList.remove("hide");
+    scoreContainer.classList.remove("hide");
+});
