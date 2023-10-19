ef7465f0b7afbf08915026875b50593409a07e13
AlmaBroman <>
2023-10-19 11:57:33 +0000

    add hover effect to all buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,27 @@
+//var for pop up
 let popUp = document.getElementById("pop-up-box");
 let popUpButton = document.getElementById("open-pop-up");
 let popUpClose = document.getElementsByClassName("close-pop-up")[0];
+//var for questions
 let questionTitle = document.getElementById("question-title");
 let questionTransl = document.getElementById("question-transl");
 let questionInfo = document.getElementById("answer-info");
 let numCorrect = 0;
 let numWrong = 0;
+//var for eventlisteners buttons
 const startButton = document.getElementById("start-button");
+const nextButton = document.getElementsByClassName("next-button")[0];
+const questionButtons = document.getElementsByClassName("question-buttons")[0];
+const restartButton = document.getElementById("restart-button");
+const homeButton = document.getElementById("home-button");
+//var for containers
 const gameTitle = document.getElementsByClassName("game-title")[0];
 const startPage = document.getElementsByClassName("start-page")[0];
 const questionContainer = document.getElementsByClassName("question-container")[0];
 const scoreContainer = document.getElementsByClassName("score-container")[0];
-const questionButtons = document.getElementsByClassName("question-buttons")[0];
 const answerContainer = document.getElementById("answer-container");
-const nextButton = document.getElementsByClassName("next-button")[0];
 const resultContainer = document.getElementsByClassName("result-container")[0];
-const restartButton = document.getElementById("restart-button");
-const homeButton = document.getElementById("home-button");
+//var for options buttons
 const kentButton = document.getElementById("opt-kent");
 const beckButton = document.getElementById("opt-beck");
 
