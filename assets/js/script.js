e351d983c1539a93124c3f5de6a0d7ab97e3eff4
AlmaBroman <>
2023-10-20 10:36:15 +0000

    delete inssuficcent functions in script, make function for displaying allo questions in html

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,9 +7,10 @@ let correctAnswer = document.getElementById("answer-is");
 let questionTitle = document.getElementById("question-title");
 let questionTransl = document.getElementById("question-transl");
 let questionInfo = document.getElementById("answer-info");
+let questionAnswer = document.getElementById("answer-is");
 //var for score/results
 let currentNumberCorrect = document.getElementById("correct-answers");
-let currentNumberWrong = document.getElementById("wrong-answers")
+let currentNumberWrong = document.getElementById("wrong-answers");
 let numCorrect = 0;
 let numWrong = 0;
 //var for eventlisteners buttons
@@ -32,24 +33,24 @@ const kentButton = document.getElementById("opt-kent");
 const beckButton = document.getElementById("opt-beck");
 
 //functions for pop up button
-popUpButton.onclick = function() {
+popUpButton.onclick = function () {
     popUp.style.display = "block";
-}
+};
 
-popUpClose.onclick = function() {
+popUpClose.onclick = function () {
     popUp.style.display = "none";
-}
+};
 
-window.onclick = function(event) {
+window.onclick = function (event) {
     if (event.target == popUp) {
         popUp.style.display = "none";
     }
-}
+};
 
 // Buttons to start the quiz.
 
 //Start button - hides startpage and un-hides questions.
-startButton.addEventListener("click", function() {
+startButton.addEventListener("click", function () {
     gameTitle.classList.add("hide");
     startPage.classList.add("hide");
     questionContainer.classList.remove("hide");
@@ -60,39 +61,35 @@ startButton.addEventListener("click", function() {
 
 //Eventlisteners to show/hide e
 //question buttons - hides buttons, shows answer.
-questionButtons.addEventListener("click", function(){
+questionButtons.addEventListener("click", function () {
     questionButtons.classList.add("hide");
     answerContainer.classList.remove("hide");
-    compareAnswer();
-    
 });
 
 //Next Button - hides answer-container
-nextButton.addEventListener("click", function() {
+nextButton.addEventListener("click", function () {
     /** 
      * if last question do: 
      * answerContainer.classList.add("hide");
-     * questionContainer.classList.add("hide");
-     * scoreContainer.classList.add("hide");
+     * questionHeader.classList.add("hide");
      * gameTitle.classList.remove("hide");
      * resultContainer.classList.remove("hide");
+     * resultMessage.classList.remove("hide");
      * else: 
      *  answerContainer.classList.add("hide");
      * questionButtons.classList.remove("hide");
      */
-    
-    /**if not last question */
+
+    //if not last question
+    questionButtons.classList.remove("hide");
     answerContainer.classList.add("hide");
-    //questionContainer.classList.add("hide");
-    //scoreContainer.classList.add("hide");
-    questionHeader.classList.add("hide");
-    gameTitle.classList.remove("hide");
-    resultContainer.classList.remove("hide");
-    resultMessage.classList.remove("hide");
+    questionsIndex++;
+    displayNextQuestion();
+    console.log(question.name)
 });
 
 //Restart button
-restartButton.addEventListener("click", function() {
+restartButton.addEventListener("click", function () {
     resultContainer.classList.add("hide");
     questionContainer.classList.remove("hide");
     scoreContainer.classList.remove("hide");
@@ -104,78 +101,28 @@ restartButton.addEventListener("click", function() {
 });
 
 // Home button
-homeButton.addEventListener("click", function() {
+homeButton.addEventListener("click", function () {
     resultContainer.classList.add("hide");
     startPage.classList.remove("hide");
     resultContainer.classList.add("hide");
     resultMessage.classList.add("hide");
-    questionContainer.classList.add("hide")
+    questionContainer.classList.add("hide");
 });
 
-function runGame() {
+let questionsIndex = 0;
 
-};
+//take question from question array
+let question = questions[questionsIndex];
+//display object properties in html
+questionTitle.innerHTML = question.name;
+questionTransl.innerHTML = question.transl;
+questionInfo.innerHTML = question.info;
+questionAnswer.innerHTML = question.answer;
 
-/**
- * displays questions + transl + info + answer
- */
-function displayQuestion() {
-    questions.forEach( 
-        (currentQuestion, questionNumber) => {
-            
-            //variables to store list items
-            const name = [];
-            const question = [];
-            const info = [];
-            const answer = [];
-            
-            questionTitle.innerHTML = currentQuestion.name;
-            questionTransl.innerHTML = currentQuestion.transl;
-            questionInfo.innerHTML = currentQuestion.info;
-            correctAnswer.innerHTML = currentQuestion.answer;
-    });
-    //make array items appear in html
-    /**
-     * questionTitle.innerHTML = currentQuestion.name;
-     * questionTransl.innerHTML = currentQuestion.transl;
-     * questionInfo.innerHTML = currentQuestion.info;
-     * correctAnswer.innerHTML = currentQuestion.answer;
-     */
-};
-displayQuestion()
-displayQuestion()
-
-/**
- * compares answer(button) with correctAnswer
- * and counts correct/wrong answers
- */
-function compareAnswer() {
-    kentButton = 'Kent';
-    beckButton = 'Beck';
-    if (kentButton === correctAnswer.innerHTML || beckButton === correctAnswer.innerHTML){
-        numCorrect ++;
-    } else {
-        numWrong ++;
-    };
-};
-//compareAnswer()
-
-//currentNumberCorrect.innerHTML = 'Correct answers: ' + numCorrect;
-//currentNumberWrong.innerHTML = 'Wrong answers: ' + numWrong;
-
-function displayStartPage() {
-
-};
-
-function displayAnswer() {
-
-};
-
-function displayScore() {
-
-};
-
-function displayResult() {
-
-};
-
+ function displayNextQuestion() {
+     question = questions[questionsIndex];
+     questionTitle.innerHTML = question.name;
+     questionTransl.innerHTML = question.transl;
+     questionInfo.innerHTML = question.info;
+     questionAnswer.innerHTML = question.answer;
+ }
